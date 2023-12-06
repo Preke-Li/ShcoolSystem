f45883b96fd538f0fa16ac742a2d1fe68c7fce37
Preke-Li <2398344098@qq.com>
2023-12-06 22:35:15 +0800

    课程表完善

--- src/main/java/com/schoolsystem/controller/CourseController.java
+++ src/main/java/com/schoolsystem/controller/CourseController.java
@@ -1,4 +1,33 @@
 package com.schoolsystem.controller;
 
+import com.schoolsystem.pojo.Course;
+import com.schoolsystem.pojo.Result;
+import com.schoolsystem.service.CourseService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
 public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("/course")
+    public Result getStudentCourse(@RequestParam("studentId") Integer studentId){
+
+        List<Course> studentCourse = courseService.getStudentCourse(studentId);
+        System.out.println("获取到的课程表为：");
+
+        for (Course course : studentCourse) {
+            System.out.println(course);
+        }
+        return Result.success(studentCourse);
+    }
 }
--- src/main/java/com/schoolsystem/service/CourseService.java
+++ src/main/java/com/schoolsystem/service/CourseService.java
@@ -1,4 +1,10 @@
 package com.schoolsystem.service;
 
+import com.schoolsystem.pojo.Course;
+
+import java.util.List;
+
 public interface CourseService {
+    List<Course> getStudentCourse(Integer studentId);
+
 }
