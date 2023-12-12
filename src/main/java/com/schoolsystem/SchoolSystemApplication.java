cd2ddb357f9f7d8cad257f14fdc26ae0f74da3dd
Preke-Li <2398344098@qq.com>
2023-12-12 17:06:25 +0800

    修复bean冲突，修改端口号为80

--- src/main/java/com/schoolsystem/SchoolSystemApplication.java
+++ src/main/java/com/schoolsystem/SchoolSystemApplication.java
@@ -1,12 +1,11 @@
 package com.schoolsystem;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.schoolsystem.*"})
-@ComponentScan(basePackages = {"com.schoolsystem.*"})
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class SchoolSystemApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/schoolsystem/controller/AdminApprovalController.java
+++ src/main/java/com/schoolsystem/controller/AdminApprovalController.java
@@ -11,7 +11,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/admin/approval")
 public class AdminApprovalController {
-
     @Autowired
     private AdminApprovalService adminApprovalService;
 
