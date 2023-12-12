81a84a42f0f253d0c79fc876f5af0fa9805ed5d3
Preke-Li <2398344098@qq.com>
2023-12-12 14:51:03 +0800

    jar包打包

--- src/main/java/com/schoolsystem/SchoolSystemApplication.java
+++ src/main/java/com/schoolsystem/SchoolSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.schoolsystem.*"})
+@ComponentScan(basePackages = {"com.schoolsystem.*"})
 public class SchoolSystemApplication {
 
     public static void main(String[] args) {
